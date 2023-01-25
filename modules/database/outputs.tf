e996317387ca55ea498037ed182abe39d7db702a
otammato <ze.qwant@gmail.com>
2023-01-25 23:07:23 +0000

    improved solution availability

--- modules/database/outputs.tf
+++ modules/database/outputs.tf
@@ -3,3 +3,11 @@
 #output "db_endpoint" {
 #  value = aws_db_instance.three_tier_db.endpoint
 #}
+
+output "db_endpoint" {
+  value = aws_rds_cluster.three_tier_db.endpoint
+}
+ 
+output "db_reader_endpoint" {
+  value = aws_rds_cluster.three_tier_db.reader_endpoint
+}
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,11 @@ output "load_balancer_endpoint" {
 #output "database_endpoint" {
 # value = module.database.db_endpoint
 #}
+
+output "database_endpoint" {
+  value = module.database.db_endpoint
+}
+ 
+output "database_reader_endpoint" {
+  value = module.database.db_reader_endpoint
+}
